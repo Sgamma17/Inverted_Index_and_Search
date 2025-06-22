e54bdfdf630404b29c74e75c0a93e8e6351388ae
Francesco Tarchi <francesco.tarchi01@gmail.com>
2025-06-22 12:41:17 +0200

    Update directory structure and files to last versions.

--- Spark/invertedindex_spark_inmappercomb.py
+++ Spark/invertedindex_spark_inmappercomb.py
@@ -2,23 +2,24 @@ import sys
 from pyspark import SparkConf, SparkContext
 import re
 import os
-from collections import defaultdict
 
 def normalize_line(line):
     return re.sub(r'[^a-z ]', ' ', line.lower())
 
-def extract_word_file_pairs(filename_content):
+def extract_word_file_pairs_combined(filename_content):
     full_path, content = filename_content
     filename = os.path.basename(full_path)
     content = normalize_line(content)
     words = content.split()
-    return [((word, filename), 1) for word in words]
 
-def in_mapper_combiner(partition):
-    local_counts = defaultdict(int)
-    for key, count in partition:
-        local_counts[key] += count
-    return local_counts.items()
+    local_count = {}
+    for word in words:
+        if word:
+            key = (word, filename)
+            local_count[key] = local_count.get(key, 0) + 1
+
+    return [(key, count) for key, count in local_count.items()]
+
 
 if __name__ == "__main__":
 
@@ -26,24 +27,23 @@ if __name__ == "__main__":
         print("Usage: inverted_index.py <input dir> <output dir>", file=sys.stderr)
         sys.exit(-1)
 
-    conf = SparkConf().setAppName("Inverted Index Spark")
+    conf = SparkConf().setAppName("Inverted Index Spark with In-Mapper Combiner")
     sc = SparkContext(conf=conf)
 
     files_rdd = sc.wholeTextFiles(sys.argv[1])
 
-    word_file_pairs = files_rdd.flatMap(extract_word_file_pairs)
+    word_file_pairs = files_rdd.flatMap(extract_word_file_pairs_combined)
 
-    combined_locally = word_file_pairs.mapPartitions(in_mapper_combiner)
-
-    word_file_counts = combined_locally.reduceByKey(lambda x, y: x + y)
+    word_file_counts = word_file_pairs.reduceByKey(lambda x, y: x + y)
 
     word_to_filenamecount = word_file_counts.map(lambda x: (x[0][0], [f"{x[0][1]}:{x[1]}"]))
 
     inverted_index = word_to_filenamecount.reduceByKey(lambda a, b: a + b)
 
-    output_lines = inverted_index.map(lambda x: f"{x[0]}\t" + "\t".join(x[1]))
-    
+    output_lines = inverted_index.map(lambda x: f"{x[0]}\t" + "\t".join((x[1])))
+
     output_lines = output_lines.sortBy(lambda line: line.split("\t")[0])
 
     output_lines.repartition(1).saveAsTextFile(sys.argv[2])
+
 
